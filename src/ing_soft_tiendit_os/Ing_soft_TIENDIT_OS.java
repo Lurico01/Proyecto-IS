b9f89bf88c5d134abf631fb213381215530dd77c
Lurico01 <131624328+Lurico01@users.noreply.github.com>
2023-05-09 23:17:08 -0600

    Add files via upload

---
+++ src/ing_soft_tiendit_os/Ing_soft_TIENDIT_OS.java
@@ -0,0 +1,12 @@
+package ing_soft_tiendit_os;
+
+public class Ing_soft_TIENDIT_OS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
